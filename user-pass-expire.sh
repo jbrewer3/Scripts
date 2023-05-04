f1f58dbd4217eda136b4082bd871e397709c33d2
JoshuaBrewer <74952163+jbrewer3@users.noreply.github.com>
2023-05-04 17:03:22 -0500

    Update user-pass-expire.sh

--- user-pass-expire.sh
+++ user-pass-expire.sh
@@ -2,26 +2,63 @@
 #create file to store username and password exp date
 touch /tmp/user-expire.txt
 #iterate through users since we are only looking at ec2-user now we specify ec2-user
-for usern in ec2-user
-do
-#get this years date in seconds
-today=$(date +%s)
-#grab the password expire date for the user
-userexpiredate=$(chage -l $usern | grep 'Password expires' |cut -d: -f2)
-# get the date the password expires in seconds
-passexp=$(date -d "$userexpiredate" "+%s")
-#calculate the difference
-exp=`expr \( $passexp - $today \)`
-#get the number of days 86400 = seconds in day
-expday=`expr \( $exp / 86400 \)`
-#echo user namd and pass exp date to txt file
-echo "$usern | $expday" > /tmp/user-expire.txt
-done
-#determine if the expire date is less than or equal to 10 days
-if [ $expday -le 10 ]
-then
-	#send notification etc echo statement below is for testing.
-	echo $expday
-else
-    echo "Password does not expire within 10 days"
-fi
+
+check_pass_exp()
+{
+	for usern in ec2-user
+	do
+	#get this years date in seconds
+	today=$(date +%s)
+	#grab the password expire date for the user
+	userexpiredate=$(chage -l $usern | grep 'Password expires' |cut -d: -f2)
+	# get the date the password expires in seconds
+	passexp=$(date -d "$userexpiredate" "+%s")
+	#calculate the difference
+	exp=`expr \( $passexp - $today \)`
+	#get the number of days 86400 = seconds in day
+	expday=`expr \( $exp / 86400 \)`
+	#echo user namd and pass exp date to txt file
+	echo "$usern | $expday" > /tmp/user-expire.txt
+	done
+	#determine if the expire date is less than or equal to 10 days
+	if [ $expday -le 10 ]
+	then
+		#send notification etc echo statement below is for testing.
+		generate_password
+	else
+    	exit 0
+	fi
+}
+
+change_password()
+{
+	sudo sh -c 'echo ec2-user:'$password' | chpasswd'
+}
+
+generate_password()
+{
+	#get all nums lower and uppercase characters
+	digits=({2..9})
+	lower=({a..k} {m..n} {p..z})
+	upper=({A..N} {P..Z})
+	#cat them
+	CharArray=(${digits[*]} ${lower[*]} ${upper[*]})
+	#generate a random number from len of array above
+	ArrayLength=${#CharArray[*]}
+	password=""
+	len = 14
+	for i in `seq 1 $length`
+	do
+        index=$(($RANDOM%$ArrayLength))
+        char=${CharArray[$index]}
+        password=${password}${char}
+	done
+	return $password
+}
+
+check_pass_exp
+
+if 
+generate_password
+
+change_password
