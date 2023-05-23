fcb085a6f1f3478a59baf0fab6c757b6f786470b
Joshua Brewer <josh-brewer@defenseunicorns.com>
2023-05-23 12:20:07 -0500

    testing push
    Signed-off-by: Joshua Brewer <josh-brewer@defenseunicorns.com>

--- EKSAMazonLINUX2setup.sh
+++ EKSAMazonLINUX2setup.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-
-#Kubectl Install and config
-curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
-chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 
